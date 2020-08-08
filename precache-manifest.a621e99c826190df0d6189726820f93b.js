fbd6abf464b4490a910cb3a4527a00aef9761541
Mary Halvorsen <mhalvorsen98@gmail.com>
2020-08-07 22:53:04 -0400

    Updates

---
+++ precache-manifest.a621e99c826190df0d6189726820f93b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89e144d0a47f095df48933eed90ee40",
+    "url": "/maryhalv/allison-personal/index.html"
+  },
+  {
+    "revision": "7e722fa48304d298ca4d",
+    "url": "/maryhalv/allison-personal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9b3b954484f043552012",
+    "url": "/maryhalv/allison-personal/static/js/2.d3b20199.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maryhalv/allison-personal/static/js/2.d3b20199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e722fa48304d298ca4d",
+    "url": "/maryhalv/allison-personal/static/js/main.85dd79f1.chunk.js"
+  },
+  {
+    "revision": "9207a938b1d030efffcc",
+    "url": "/maryhalv/allison-personal/static/js/runtime-main.a958fb76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/maryhalv/allison-personal/static/media/logo.5d5d9eef.svg"
+  }
+]);
